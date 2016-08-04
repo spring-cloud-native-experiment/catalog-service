de291ee992f9a4e8510478d269815c4a2d3361fd
Praveer Gupta <praveerg@inpraveerg.local>
2016-08-04 11:49:28 +0530

    Add the minimum things required to bring up a rest service

---
+++ src/main/java/com/example/author/AuthorServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.author;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorServiceApplication.class, args);
+	}
+}
