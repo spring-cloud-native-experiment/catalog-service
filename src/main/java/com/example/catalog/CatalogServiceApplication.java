4d23a16c46f9914ef67d7f9057fae6a226d0913b
Jagpreet Singh Tamber <jagpreetstamber@gmail.com>
2016-08-28 22:20:57 +0530

    Add oauth2 authorization for the resource

--- src/main/java/com/example/catalog/CatalogServiceApplication.java
+++ src/main/java/com/example/catalog/CatalogServiceApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableResourceServer
 public class CatalogServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CatalogServiceApplication.class, args);
